9badc12077b73d3ee6f71743166a70e0ac01c40d
sasukecodes <44441646+sasukecodes@users.noreply.github.com>
2020-06-12 17:36:15 +0530

    Add files via upload

---
+++ Models/activity.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+
+
+namespace projectmain.Models
+{
+    [Table("activitytoday")]
+    public class activity 
+    {
+        [PrimaryKey,Unique]
+        public string Interval { get; set; }
+       
+        /// <summary>
+        /// incommplete
+        /// </summary>
+       
+        public string Name { get; set; }
+
+        /// <summary>
+        /// incommplete
+        /// </summary>
+        
+        public string Category { get; set; }
+
+        public int Categoryno { get; set; }
+
+    }
+}
